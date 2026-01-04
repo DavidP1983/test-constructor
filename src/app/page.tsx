04a2be84781a71c2283b4c8a98b7e4a3c92d98f6
David <David.Piruzashvili@gmail.com>
2026-01-04 13:49:36 +0400

    fix Login form page, added Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import { LoginForm } from "@/features/auth/login/ui/LoginForm";
+import { Spinner } from "@/shared/ui/spinner/Spinner";
+import { Metadata } from "next";
+import { Suspense } from "react";
+
+
+export const metadata: Metadata = {
+  title: "Form Page",
+  description: "Login form page",
+};
+
 
 export default function Home() {
   return (
     <>
-      <LoginForm />
+      <Suspense fallback={<Spinner isFallback={true} />}>
+        <LoginForm />
+      </Suspense>
     </>
   );
 }
