80e409fe07e82c21654454f7d5fa90fe7ec21405
David <David.Piruzashvili@gmail.com>
2026-03-05 18:58:31 +0400

    Implement email notification

--- src/entities/completed-test/ui/TestSummary.tsx
+++ src/entities/completed-test/ui/TestSummary.tsx
@@ -1,5 +1,6 @@
 import { TestPassResultProps } from "@/widgets/test-result/ui/TestPassResult";
 import clsx from "clsx";
+import { formatDate, formatDuration } from "../utils/formatter";
 
 
 import styles from '@/styles/blocks/summary.module.scss';
@@ -19,40 +20,32 @@ export const TestSummary = ({ completedTest }: Omit<TestPassResultProps, "origin
     } = completedTest ?? {};
 
 
-    const formatDuration = (seconds: number | null | undefined): string => {
-        if (!seconds || seconds < 0) return "00:00:00";
-
-        const hrs = Math.floor(seconds / 3600);
-        const mins = Math.floor((seconds % 3600) / 60);
-        const secs = seconds % 60;
-        return `${String(hrs).padStart(2, '0')}:${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
-    }
-
+    const formattedDuration = formatDuration(duration);
+    const formattedDate = formatDate(completedAt);
     const incorrectAnswers = completedTest.answers.length - correctAnswers;
 
     return (
-        <section aria-labelledby="test summary" className={styles.summary}>
-            <div className="container">
-                <div className={styles.summary__center}>
-                    <div className={styles.summary__content}>
-                        <h2 className={clsx('title', styles.summary__title)}>Test Summary</h2>
-                        <ul className={styles.summary__desc}>
-                            <li><strong>Candidate: </strong><span>{candidateName}</span></li>
-                            <li><strong>Candidate Email: </strong><span>{candidateEmail}</span></li>
-                            <li><strong>Test: </strong><span>{testName}</span></li>
-                            <li><strong>Status:</strong><span>{status === 'passed' ? 'PASSED ✅ ' : 'FAILED ❌'}</span></li>
-                            <li><strong>Score: </strong><span>{score}%</span></li>
-                            <li><strong>Correct: </strong><span>{correctAnswers} / {totalQuestions}</span></li>
-                            <li><strong>Duration: </strong><span>{formatDuration(duration)}</span></li>
-                            <li><strong>Completed: </strong><span>{completedAt}</span></li>
-                        </ul>
-                        <ul className={styles.summary__result}>
-                            <li><span>🟢</span>Correct answers: {correctAnswers}</li>
-                            <li><span>🔴 </span>Incorrect answers: {incorrectAnswers}</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <div className={styles.summary__block}>
+            <h2 className={clsx('title', styles.summary__title)}>Test Summary</h2>
+            <ul className={styles.summary__desc}>
+                <li><strong>Candidate: </strong><span>{candidateName}</span></li>
+                <li><strong>Candidate Email: </strong><span>{candidateEmail}</span></li>
+                <li><strong>Test: </strong><span>{testName}</span></li>
+                <li><strong>Status:</strong><span>{status === 'passed' ? 'PASSED ✅ ' : 'FAILED ❌'}</span></li>
+                <li><strong>Score: </strong><span>{score}%</span></li>
+                <li><strong>Correct: </strong><span>{correctAnswers} / {totalQuestions}</span></li>
+                <li><strong>Duration: </strong><span>{formattedDuration}</span></li>
+                <li><strong>Completed: </strong><span>{completedAt}</span></li>
+            </ul>
+            <ul className={styles.summary__result}>
+                <li><span>🟢</span>Correct answers: {correctAnswers}</li>
+                <li><span>🔴 </span>Incorrect answers: {incorrectAnswers}</li>
+            </ul>
+            {status === 'failed' &&
+                (
+                    <span className={clsx("icon-warning-empty", styles.summary__waring)}>Candidate has already received a failure notification email,  <strong>{formattedDate}</strong>.</span>
+                )
+            }
+        </div>
     )
 }
